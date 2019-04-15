7d57c9097022496ae1489a3e1cb4e958f2045608
Arti Zirk <arti.zirk@gmail.com>
2019-04-15 15:56:01 +0300

    Comment out gcc-musl

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ CC = gcc
 CFLAGS = -I./src -g
 
 # Want to build with musl? run make getheaders and uncomment lines below
-CFLAGS += -Ikernel-headers/$(shell uname -m)/include
-CC = musl-gcc
+#CFLAGS += -Ikernel-headers/$(shell uname -m)/include
+#CC = musl-gcc
